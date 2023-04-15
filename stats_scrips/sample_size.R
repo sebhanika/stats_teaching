0bfa38efa8e819a4d72776af1875c57bf6187542
sebhanika <hanikasebastian@gmail.com>
2023-04-15 11:17:23 +0200

    setting up sample size script

---
+++ stats_scrips/sample_size.R
@@ -0,0 +1,22 @@
+## ---------------------------
+##
+## Title: Sample size example
+##
+## Topic: Shows the effect of sample size on simple calculations
+##
+## Author: Sebastian Hanika
+##
+## Date Created: 2023-04-15
+##
+## ---------------------------
+##
+## Notes:
+##   
+##
+## ---------------------------
+
+
+
+# Libraries ---------------------------------------------------------------
+
+library(tidyverse)
