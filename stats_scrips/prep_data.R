85c4bddef1e1d92c79b55aec111f7ce2eaf15096
sebhanika <hanikasebastian@gmail.com>
2023-05-30 17:01:06 +0200

    formating code

--- stats_scrips/prep_data.R
+++ stats_scrips/prep_data.R
@@ -22,25 +22,30 @@ library(geojsonsf)
 library(sf)
 library(eurostat)
 library(purrr)
+library(downloader)
 
 # geodata -----------------------------------------------------------------
 
+# download data
+url_map <- "https://gisco-services.ec.europa.eu/distribution/v2/nuts/geojson/NUTS_RG_20M_2021_4326.geojson" # nolint: line_length_linter.
 
-# download data from https://ec.europa.eu/eurostat/web/gisco/geodata/reference-data/administrative-units-statistical-units/nuts
+download(
+  url = url_map,
+  dest = "data/NUTS_RG_20M_2021_4326.geojson",
+  mode = "wb"
+) # downloads zip folder into current directory
 
-europe_bb <- c(-10.5, 36, 42, 70)
-
-nuts2.raw <- geojson_sf("data/NUTS_RG_20M_2021_4326.geojson")
-nuts2 <- nuts2.raw %>%
+# prep data
+nuts2 <- geojson_sf("data/NUTS_RG_20M_2021_4326.geojson") %>%
   filter(LEVL_CODE == 2) %>%
-  dplyr::filter(!grepl("^FRY|^FR$", NUTS_ID)) %>% # exclude French oversees terretories
+  filter(!grepl("^FRY|^FR$", NUTS_ID)) %>% # Exclude Oversee territories
   rename(geo = NUTS_ID) %>%
   select(-c(NUTS_NAME, MOUNT_TYPE, COAST_TYPE, FID, LEVL_CODE))
 
 # Download data -----------------------------------------------------------
 
 # names of datasets
-names.eurostat <- c(
+names_eurostat <- c(
   "demo_r_d2jan", "demo_r_pjanind2", "nama_10r_2gdp",
   "edat_lfse_04", "rd_e_gerdreg", "rd_e_gerdreg",
   "htec_emp_reg2", "lfst_r_lfu3rt", "lfst_r_lfe2ehour",
@@ -49,53 +54,53 @@ names.eurostat <- c(
 
 # Define a function to download Eurostat data
 download_eurostat <- function(dataset_name) {
-  # Download the data with the time_format = "num" argument
   data <- get_eurostat(dataset_name, time_format = "num")
 
-  # Return the data
   return(data)
 }
 
 # Load each dataset using lapply
-dat.eurostat <- lapply(names.eurostat, download_eurostat)
+dat_eurostat <- lapply(names_eurostat, download_eurostat)
 
-names(dat.eurostat) <- names.eurostat
-
+names(dat_eurostat) <- names_eurostat
 
 
 # Temp dave data ----------------------------------------------------------
 
-# here I temporarly save and reload the data so it becomes quicker to access between sessions.
-# will be deleted later
+# here I temporarly save and reload the data so it becomes
+# quicker to access between sessions will be deleted later
 
-lapply(names(dat.eurostat), function(df) {
-  df_name <- file.path("data", paste0(df, ".rds")) # create a file path in the "data" folder
-  saveRDS(dat.eurostat[[df]], file = df_name) # save the current data frame to its own RDS file
+lapply(names(dat_eurostat), function(df) {
+  df_name <- file.path("data", paste0(df, ".rds"))
+  saveRDS(dat_eurostat[[df]], file = df_name)
 })
 
 # load data
-names.eurostat <- list.files(path = "data", pattern = ".rds", full.names = TRUE)
+names_eurostat <- list.files(path = "data", pattern = ".rds", full.names = TRUE)
 
-dat.eurostat <- names.eurostat %>% map(readRDS)
+dat_eurostat <- names_eurostat %>% map(readRDS)
 
-names(dat.eurostat) <- gsub("data/|\\.rds", "", names.eurostat)
+names(dat_eurostat) <- gsub("data/|\\.rds", "", names_eurostat)
 
 
 # data cleaning -----------------------------------------------------------
 
-
 ### Regio data
-pop.grw <- dat.eurostat[["demo_r_d2jan"]] %>%
+pop_grw <- dat_eurostat[["demo_r_d2jan"]] %>%
   filter(
     sex == "T",
     age == "TOTAL",
     time %in% c(2021, 2016)
   ) %>%
-  pivot_wider(values_from = values, names_from = time, names_prefix = "pop_") %>%
+  pivot_wider(
+    values_from = values,
+    names_from = time,
+    names_prefix = "pop_"
+  ) %>%
   mutate(popgrw_2016_2021 = ((pop_2021 - pop_2016) / pop_2016) * 100)
 
 
-pop.ind <- dat.eurostat[["demo_r_pjanind2"]] %>%
+pop_ind <- dat_eurostat[["demo_r_pjanind2"]] %>%
   filter(
     indic_de %in% c("MEDAGEPOP", "DEPRATIO1"),
     time == 2021
@@ -106,7 +111,7 @@ pop.ind <- dat.eurostat[["demo_r_pjanind2"]] %>%
   )
 
 
-gdp <- dat.eurostat[["nama_10r_2gdp"]] %>%
+gdp <- dat_eurostat[["nama_10r_2gdp"]] %>%
   filter(
     time == 2021,
     unit %in% c(
@@ -122,7 +127,7 @@ gdp <- dat.eurostat[["nama_10r_2gdp"]] %>%
 
 
 # total expendiature on r&d
-gerd <- dat.eurostat[["rd_e_gerdreg"]] %>%
+gerd <- dat_eurostat[["rd_e_gerdreg"]] %>%
   filter(
     time == 2019,
     unit %in% c(
@@ -131,29 +136,21 @@ gerd <- dat.eurostat[["rd_e_gerdreg"]] %>%
     ) # euro per inhabitant
   ) %>%
   group_by(geo, time, unit) %>%
-  summarize(values = sum(values, na.rm = T)) %>%
+  summarize(values = sum(values, na.rm = TRUE)) %>%
   ungroup() %>%
   pivot_wider(values_from = values, names_from = unit, names_prefix = "gerd_")
 
 
-htch_jobs <- dat.eurostat[["htec_emp_reg2"]] %>%
+# hightech jobs
+htch_jobs <- dat_eurostat[["htec_emp_reg2"]] %>%
   filter(time == 2019, nace_r2 == "HTC", unit == "PC_EMP", sex == "T") %>%
   select(-c(unit, time)) %>%
   pivot_wider(values_from = values, names_from = nace_r2) %>%
   rename(HTC_2019 = HTC)
 
-plot(density(htch_jobs$HTC_2019, na.rm = T))
 
-
-
-
-### Employ data, sex and age
-
-
-# constrct common age group across all geos and time in 2021
-
-
-edu <- dat.eurostat[["edat_lfse_04"]] %>%
+# education
+edu <- dat_eurostat[["edat_lfse_04"]] %>%
   filter(
     time == 2021,
     age == "Y25-64",
@@ -168,7 +165,7 @@ edu <- dat.eurostat[["edat_lfse_04"]] %>%
 
 
 # unemp
-unemp <- dat.eurostat[["lfst_r_lfu3rt"]] %>%
+unemp <- dat_eurostat[["lfst_r_lfu3rt"]] %>%
   filter(
     isced11 == "TOTAL",
     time == 2021,
@@ -177,8 +174,9 @@ unemp <- dat.eurostat[["lfst_r_lfu3rt"]] %>%
   ) %>%
   rename(unemp_pc = values)
 
+
 # hours worked
-hours_wrk <- dat.eurostat[["lfst_r_lfe2ehour"]] %>%
+hours_wrk <- dat_eurostat[["lfst_r_lfe2ehour"]] %>%
   filter(time == 2021, age == "Y25-64") %>%
   pivot_wider(
     names_from = sex, names_prefix = "hrs_",
@@ -187,7 +185,7 @@ hours_wrk <- dat.eurostat[["lfst_r_lfe2ehour"]] %>%
   mutate(hrs_gap = hrs_M - hrs_F)
 
 
-emp <- dat.eurostat[["lfst_r_lfe2en2"]] %>%
+emp <- dat_eurostat[["lfst_r_lfe2en2"]] %>%
   filter(
     time == 2021,
     age == "Y25-64",
@@ -195,8 +193,8 @@ emp <- dat.eurostat[["lfst_r_lfe2en2"]] %>%
     nace_r2 %in% c(
       "B-E", # Industry
       "G-I", # Retail and low-skill services
-      "M_N"
-    ) # Knowdlege industry
+      "M_N" # Knowdlege industry
+    )
   ) %>%
   mutate(nace_r2 = case_when(
     nace_r2 == "B-E" ~ "Industry",
@@ -210,7 +208,7 @@ emp <- dat.eurostat[["lfst_r_lfe2en2"]] %>%
 
 
 # life exptectancy data
-le <- dat.eurostat[["demo_r_mlifexp"]] %>%
+le <- dat_eurostat[["demo_r_mlifexp"]] %>%
   filter(age == "Y1", time == 2021) %>%
   pivot_wider(
     names_from = sex, names_prefix = "le_",
@@ -222,17 +220,11 @@ le <- dat.eurostat[["demo_r_mlifexp"]] %>%
 
 
 
-data.try <- nuts2 %>%
+data_try <- nuts2 %>%
   as.data.frame() %>%
   select(c(geo, URBN_TYPE, NAME_LATN)) %>%
-  left_join(select(pop.grw, c(geo, popgrw_2016_2021)), by = "geo") %>%
-  left_join(select(pop.ind, c(geo, MEDAGEPOP_YR, DEPRATIO1_PC)), by = "geo") %>%
+  left_join(select(pop_grw, c(geo, popgrw_2016_2021)), by = "geo") %>%
+  left_join(select(pop_ind, c(geo, MEDAGEPOP_YR, DEPRATIO1_PC)), by = "geo") %>%
   left_join(select(gdp, c(geo, gdp_EUR_HAB, gdp_MIO_PPS_EU27_2020)), by = "geo") %>%
   left_join(select(gerd, c(geo, gerd_EUR_HAB, gerd_MIO_EUR)), by = "geo") %>%
   left_join(select(htch_jobs, c(geo, HTC_2019)), by = "geo")
-
-
-
-
-
-EUR_HAB
